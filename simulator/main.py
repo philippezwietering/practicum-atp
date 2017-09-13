65714aaae988e74ac8d919ae88196745cc1c5cf0
philippezwietering <philippe.zwietering@gmail.com>
2017-09-13 12:41:34 +0200

    Added portfolio stuff week 1 and logging information on controller module

--- simulator/main.py
+++ simulator/main.py
@@ -1,5 +1,36 @@
 if __name__ == "__main__":
     """Only perform actions when invoked directly!"""
     from Simulator import Simulator
+    from Controller import Controller
+    import inspect
+    from functools import wraps
+
+    def log(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            try:
+                print("Entering: [%s] with parameters %s" % (func.__name__, args))
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print("Exception in %s : %s" % (func.__name__, e))
+            finally:
+                print("Exiting: [%s]" % func.__name__)
+        return wrapped
+
+    def trace(cls):
+        for name, m in inspect.getmembers(cls, inspect.isfunction):
+            setattr(cls, name, log(m))
+        return cls
+
+    Simulator = trace(Simulator)
+    Controller = trace(Controller)
     simulator = Simulator(True) # use Simulator(False) to disable the GUI
+
+    #print(simulator)
     simulator.run()
+    # print('Dir of simulator: ', dir(simulator), '\n')
+    # print("Dir of simulator controller: ", dir(simulator._Simulator__controller), '\n')
+    # print("Vars of simulator controller: ", vars(simulator._Simulator__controller), "\n")
+    # print("Dir of simulator controller sensor: ", dir(simulator._Simulator__controller._Controller__sensors), "\n")
+    # print("Dir of simulator controller effector: ", dir(simulator._Simulator__controller._Controller__effectors), "\n")
