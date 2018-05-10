a8fcb7178fd52949cb5c6c659033db3aca500c0e
philippezwietering <philippe.zwietering@gmail.com>
2018-05-10 15:47:26 +0200

    Cleanup for laptop reinstall

---
+++ simulator/main.py
@@ -0,0 +1,43 @@
+if __name__ == "__main__":
+    """Only perform actions when invoked directly!"""
+    from Simulator import Simulator
+    from Controller import Controller
+    import inspect
+    from functools import wraps
+
+    def log(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            try:
+                print("Entering: [%s] with parameters %s" % (func.__name__, args))
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print("Exception in %s : %s" % (func.__name__, e))
+            finally:
+                print("Exiting: [%s]" % func.__name__)
+        return wrapped
+
+    def trace(cls):
+        for name, m in inspect.getmembers(cls, inspect.isfunction):
+            setattr(cls, name, log(m))
+        return cls
+
+    Simulator = trace(Simulator)
+    Controller = trace(Controller)
+    simulator = Simulator(True) # use Simulator(False) to disable the GUI
+
+    #lijstje = dir(Controller)
+	for l in lijstje:
+	    x = eval("Controller." + l)
+	    if inspect.isclass(x):
+	        print(inspect.signature(x))
+	        print(inspect.getdoc(x))
+
+    #print(simulator)
+    simulator.run()
+    # print('Dir of simulator: ', dir(simulator), '\n')
+    # print("Dir of simulator controller: ", dir(simulator._Simulator__controller), '\n')
+    # print("Vars of simulator controller: ", vars(simulator._Simulator__controller), "\n")
+    # print("Dir of simulator controller sensor: ", dir(simulator._Simulator__controller._Controller__sensors), "\n")
+    # print("Dir of simulator controller effector: ", dir(simulator._Simulator__controller._Controller__effectors), "\n")
